00c393f7399b25dcb43a9c97d5587d98435652eb
Rebe <Rebe@DESKTOP-FQRJBO2>
2024-02-26 11:23:08 +0100

    Primer commit

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,89 @@
+package cuentas;
+
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        this.nombre =nom;
+        this.cuenta=cue;
+        this.saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        this.saldo = this.saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        this.saldo = this.saldo - cantidad;
+    }
+    
+    public void operativa_cuenta(float cantidad)
+    {
+      try {
+            retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+}
